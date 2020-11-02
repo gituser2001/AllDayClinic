617b03f322a51305fa15dc1c6259b5e1de04c27c
gituser2001 <64527203+gituser2001@users.noreply.github.com>
2020-11-02 15:18:23 +0000

    Initial commit

---
+++ app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.example.ipca.gamecatalog.alldayclinic
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.hide();
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val handler = Handler()
+        handler.postDelayed({ mostrarMainActivity() }, 3000)
+    }
+
+    private fun mostrarMainActivity() {
+        val intent = Intent(this@SplashScreen, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
