1c8dc23c18e1fd76e6bdd3479a9cea6ec573812e
gituser2001 <64527203+gituser2001@users.noreply.github.com>
2020-11-03 15:54:02 +0000

    Erro no Login

--- app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/MainActivity.kt
+++ app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.example.ipca.gamecatalog.alldayclinic
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.ipca.gamecatalog.alldayclinic.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 
-
 class MainActivity : AppCompatActivity() {
+private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         supportActionBar?.hide();
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        auth.createUserWithEmailAndPassword(binding.editTextUtente.text.toString(),
+            binding.editTextPassword.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    val intent = Intent(this, Agenda::class.java)
+                    intent.putExtra("email", binding.editTextUtente.text.toString())
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                } else {
+
+                }
+            }
+
 
     }
-    
+
+
 }
