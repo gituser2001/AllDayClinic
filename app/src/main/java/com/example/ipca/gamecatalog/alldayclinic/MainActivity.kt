f453ced5f6caaf144514cac435cab1f5c0264650
SirNoura <65782557+SirNoura@users.noreply.github.com>
2020-11-03 20:13:38 +0000

    Login design done

--- app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/MainActivity.kt
+++ app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/MainActivity.kt
@@ -4,26 +4,28 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ipca.gamecatalog.alldayclinic.databinding.ActivityMainBinding
+
+
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
 
-
         supportActionBar?.hide();
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
         auth = FirebaseAuth.getInstance()
         auth.createUserWithEmailAndPassword(binding.editTextUtente.text.toString(),
-            binding.editTextPassword.text.toString())
+          binding.editTextPassword.text.toString())
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     val user = auth.currentUser
                     val intent = Intent(this, Agenda::class.java)
-                    intent.putExtra("e-mail", binding.editTextUtente.text.toString())
+                    intent.putExtra("email", binding.editTextUtente.text.toString())
                     intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
                     startActivity(intent)
                 } else {
