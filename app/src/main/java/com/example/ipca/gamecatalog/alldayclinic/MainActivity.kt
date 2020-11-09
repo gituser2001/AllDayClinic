b358c7bee747d8cef39d38f20e7afc4c8680532a
peterutoKR <50056222+peterutoKR@users.noreply.github.com>
2020-11-09 15:29:23 +0000

    Added val btnLogin

--- app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/MainActivity.kt
+++ app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.ipca.gamecatalog.alldayclinic
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuth.*
 
-class MainActivity : AppCompatActivity() {
+class
+MainActivity : AppCompatActivity() {
 private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -17,21 +20,23 @@ private lateinit var auth : FirebaseAuth
 
         val email = findViewById<TextView>(R.id.editTextUtente)
         val password = findViewById<TextView>(R.id.editTextPassword)
-        auth = getInstance()
-        auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    val user = auth.currentUser
-                    val intent = Intent(this, Agenda::class.java)
-                    intent.putExtra("email", email.text.toString())
-                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    startActivity(intent)
-                } else {
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+        btnLogin.setOnClickListener {  auth = getInstance()
+            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            val user = auth.currentUser
+                            val intent = Intent(this, Agenda::class.java)
+                            intent.putExtra("email", email.text.toString())
+                            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this@MainActivity, "ERROU", Toast.LENGTH_SHORT).show()
+                        }
 
-                }
 
+                    }}
 
-            }
     }
 
 }
