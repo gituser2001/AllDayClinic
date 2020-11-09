a291f9070a98c793a389cd1483b49a92910fae7f
rowst07 <51093341+rowst07@users.noreply.github.com>
2020-11-09 21:11:02 +0000

    Mudança Design Geral

--- app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/SplashScreen.kt
+++ app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/SplashScreen.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatDelegate
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         supportActionBar?.hide();
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
