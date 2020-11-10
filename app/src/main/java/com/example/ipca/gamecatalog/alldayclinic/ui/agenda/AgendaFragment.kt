816da6d04e88df4d542bf7f552a61d8ad52389da
peterutoKR <50056222+peterutoKR@users.noreply.github.com>
2020-11-10 20:43:38 +0000

    Refactor da classe AgendaFragment.kt e AgendaViewModel.kt.
    Refactor id text_home para text_agenda.

--- app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/ui/agenda/AgendaFragment.kt
+++ app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/ui/agenda/AgendaFragment.kt
@@ -1,4 +1,4 @@
-package com.example.ipca.gamecatalog.alldayclinic.ui.home
+package com.example.ipca.gamecatalog.alldayclinic.ui.agenda
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.ipca.gamecatalog.alldayclinic.R
 
-class HomeFragment : Fragment() {
+class AgendaFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var agendaViewModel: AgendaViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        agendaViewModel =
+            ViewModelProvider(this).get(AgendaViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_agenda, container, false)
+        val textView: TextView = root.findViewById(R.id.text_agenda)
+        agendaViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/ui/agenda/AgendaViewModel.kt
+++ app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/ui/agenda/AgendaViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.ipca.gamecatalog.alldayclinic.ui.home
+package com.example.ipca.gamecatalog.alldayclinic.ui.agenda
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class AgendaViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
 
