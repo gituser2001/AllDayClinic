f20ed8de2b8893b087c80d1ad2d6bdab1f1cd0e0
peterutoKR <50056222+peterutoKR@users.noreply.github.com>
2020-11-11 23:56:00 +0000

    Acrescento de alguns ficheiros na pasta drawable tais como: -no_image2.jpeg- Imagem para teste do chat. -shape_bg_incoming_bubble.xml e shape_bg_outgoing_bubble.xml- xml para modelar uma forma circular na imagem escolhida por ambos os utilizadores. -ic_message_black_24dp.xml- xml para sublinhar a mensagem a preto para sinalizar que foi recebida e não foi lida.
    Acrescento de alguns layouts na pasta layout tais como:
    -chat_mensagem_enviada.xml- ficheiro xml referente á parte que diz respeito ao design de quando o utilizador envia uma mensagem.
    -chat_mensagem_recebida.xml- ficheiro xml referente á parte que diz respeito ao design de quando o utilizador recebe uma mensagem.
    -chat_nova_mensagem.xml- ficheiro xml referente á parte que mostra uma mensagem recebida na zona dos chats.
    -chat_ultimas_mensagens.xml-ficheiro xml referente ás últimas mensagens recebidas.
    -chat_utilizador_contacto.xml-ficheiro xml referente ao nome do utilizador que enviou mensagem.
    
    Acrescento de uma dependency:
    -implementation 'de.hdodenhof:circleimageview:3.1.0'- dependency para poder utilizar a forma circular no ambiente do chat.
    
    Acrescento da pasta de animações:
    -Animações para o chat.

--- app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/ui/chat/ChatFragment.kt
+++ app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/ui/chat/ChatFragment.kt
@@ -2,18 +2,14 @@ package com.example.ipca.gamecatalog.alldayclinic.ui.chat
 
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.ipca.gamecatalog.alldayclinic.R
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 
 class ChatFragment : Fragment() {
 
