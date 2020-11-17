dcbe40b9926efa541af10eafd17fb95d24d9892a
SirNoura <65782557+SirNoura@users.noreply.github.com>
2020-11-17 02:46:38 +0000

    adicionei um valor Year para calcular idades de pacientes

--- app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/ui/profile/ProfileFragment.kt
@@ -1,10 +1,15 @@
 package com.example.ipca.gamecatalog.alldayclinic.ui.profile
 
+
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.makeText
+
 import androidx.fragment.app.Fragment
 import com.example.ipca.gamecatalog.alldayclinic.R
 import com.example.ipca.gamecatalog.alldayclinic.profile
@@ -13,6 +18,9 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
+import java.util.*
+import kotlin.collections.HashMap
+
 
 class ProfileFragment : Fragment() {
 private lateinit var auth : FirebaseAuth
@@ -20,16 +28,16 @@ private lateinit var auth : FirebaseAuth
 private var currentUser : profile? = null
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         val uid  = FirebaseAuth.getInstance().uid
         val db = FirebaseFirestore.getInstance()
         //val reference = FirebaseFirestore.getInstance()
             //.collection("users")
            // .document(uid.toString())
-
+        val year: Int =  Calendar.getInstance().get(Calendar.YEAR);
         val root = inflater.inflate(R.layout.fragment_profile, container, false)
         val text_nome : TextView = root.findViewById(R.id.text_name)
         val text_idade : TextView = root.findViewById(R.id.text_age)
@@ -43,9 +51,9 @@ private var currentUser : profile? = null
                         querySnapshot?.data?.let {
                             listUser = profile.fromHash(querySnapshot.data as HashMap<String, Any?>)
                             listUser?.let{ user ->
-                                var dtaNas = 2020 - (user.dtaNasc?.toInt()!!)
+                                var dtaNas = (year - user.dtaNasc?.toInt()!!)
                                 text_nome.setText(user.nome)
-                                text_idade.setText(dtaNas.toString())
+                                text_idade.setText("Idade : " + dtaNas.toString())
                             }
                         }
                     }
