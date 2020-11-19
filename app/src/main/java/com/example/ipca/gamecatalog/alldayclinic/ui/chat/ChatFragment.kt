1df930572f27d6e050daeef51856030563ef13ba
peterutoKR <50056222+peterutoKR@users.noreply.github.com>
2020-11-19 17:30:46 +0000

    Cenas chat feitas por Alexandre Pereira e João Gouveia

--- app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/ui/chat/ChatFragment.kt
+++ app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/ui/chat/ChatFragment.kt
@@ -7,34 +7,38 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.ipca.gamecatalog.alldayclinic.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class ChatFragment : Fragment() {
-
-    private lateinit var chatViewModel: ChatViewModel
-
-    //não remover que poderá ser necessário.
-    companion object
-    {
-        const val USER_KEY = "USER_KEY"
-        private val TAG = ChatFragment::class.java.simpleName
-    }
+    private lateinit var auth : FirebaseAuth
 
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        chatViewModel =
-            ViewModelProvider(this).get(ChatViewModel::class.java)
+        //var view: View = inflater.inflate(R.layout.chat_ultimas_mensagens.container.false)
+        super.onCreate(savedInstanceState)
+        //setC(R.layout.news_articles)
+
+
+
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
         val root = inflater.inflate(R.layout.fragment_chat, container, false)
         val textView: TextView = root.findViewById(R.id.text_chat)
-        chatViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+            val swiperefresh : SwipeRefreshLayout = root.findViewById(R.id.swiperefresh)
+
+         //   swiperefresh.setOnRefreshListener {
+           //     fetchCurrentUser()
+             //   listenForLatestMessages()
+            //}
 
         return root
 
