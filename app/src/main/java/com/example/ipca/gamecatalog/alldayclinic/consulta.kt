54328765f5c1c576a3a5903f49b72e1d9c8c7191
peterutoKR <50056222+peterutoKR@users.noreply.github.com>
2020-12-02 17:38:25 +0000

    -Implementação do groupie. -Exclusão da classe User.kt. -Comentários em consulta.kt em todos os pontos referentes a data. -Início de desenvolvimento do adapter. -Refactor do id recyclerview_new_messsage.

--- app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/consulta.kt
+++ app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/consulta.kt
@@ -3,27 +3,26 @@ package com.example.ipca.gamecatalog.alldayclinic
 import com.google.firebase.auth.FirebaseUser
 import com.google.type.DateTime
 import java.sql.Array
-import java.sql.Timestamp
 
 class consulta {
 
     var idUser : String? = null
     var idMedico : String? = null
-    //var data : com.google.firebase.Timestamp? = null
+  //  var data : Long? = null
     var sala : String? = null
     var tipoConsulta : String? = null
 
     constructor(
             idUser: String?,
             idMedico: String?,
-            //data: com.google.firebase.Timestamp?,
+         //   data: Long?,
             sala: String?,
             tipoConsulta: String?
 
     ) {
         this.idUser = idUser
         this.idMedico = idMedico
-        //this.data    =   data
+      //  this.data    =   data
         this.sala = sala
         this.tipoConsulta = tipoConsulta
 
@@ -33,7 +32,7 @@ class consulta {
         val hasMap = HashMap<String, Any?>()
         hasMap["idUser"] = idUser
         hasMap["nomeMedico"] = idMedico
-        //hasMap["data"] = data
+      //  hasMap["data"] = data
         hasMap["sala"] = sala
         hasMap["tipoConsulta"] = tipoConsulta
 
@@ -45,7 +44,7 @@ class consulta {
             val item = consulta(
                     hashMap["idUser"].toString(),
                     hashMap["idMedico"].toString(),
-                    //hashMap["data"] as com.google.firebase.Timestamp,
+                  //  hashMap["data"] as Long,
                     hashMap["sala"].toString(),
                     hashMap["tipoConsulta"].toString()
             )
--- app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/ui/chat/NewMessageActivity.kt
+++ app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/ui/chat/NewMessageActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ipca.gamecatalog.alldayclinic.R
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
 
 class NewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,8 +18,23 @@ class NewMessageActivity : AppCompatActivity() {
         supportActionBar?.title = "Escolha um utilizador"
 
 
-         //recycler_view_new_message.adapter
+        val adapter = GroupAdapter<GroupieViewHolder>()
+        val recyclerview_new_message = findViewById<RecyclerView>(R.id.recyclerview_new_message)
 
+        //adapter.add(??)
 
+        recyclerview_new_message.adapter = adapter
     }
 }
+
+/*class profile : Item<RecyclerView.ViewHolder>()
+{
+    override fun bind(viewHolder: RecyclerView.ViewHolder, position: Int)
+    {
+
+    }
+
+    override fun getLayout(): Int {
+        return R.layout
+    }
+}*/
