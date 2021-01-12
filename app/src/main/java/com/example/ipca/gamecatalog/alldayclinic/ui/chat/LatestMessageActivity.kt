df452329426ed66a78b9942c46fd8399d9c72944
SirNoura <65782557+SirNoura@users.noreply.github.com>
2021-01-12 17:28:17 +0000

    Chat concluido

--- app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/ui/chat/LatestMessageActivity.kt
+++ app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/ui/chat/LatestMessageActivity.kt
@@ -3,12 +3,14 @@ package com.example.ipca.gamecatalog.alldayclinic.ui.chat
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Message
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.ipca.gamecatalog.alldayclinic.MainActivity
+import com.example.ipca.gamecatalog.alldayclinic.Message
 import com.example.ipca.gamecatalog.alldayclinic.R
 import com.example.ipca.gamecatalog.alldayclinic.profile
 import com.example.ipca.gamecatalog.alldayclinic.ui.chat.NewMessageActivity.Companion.USER_KEY
@@ -22,6 +24,8 @@ import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_latest_message.*
 import kotlinx.android.synthetic.main.chat_ultimas_mensagens.view.*
+import kotlinx.android.synthetic.main.chat_ultimas_mensagens.view.username_textview_latest_message
+import kotlinx.android.synthetic.main.user_row_new_message.view.*
 
 class LatestMessageActivity : AppCompatActivity() {
     companion object {
@@ -80,14 +84,16 @@ class LatestMessageActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
-    class LatestMessageRow(val chatMessage: com.example.ipca.gamecatalog.alldayclinic.Message) :
-        Item<GroupieViewHolder>() {
+    class LatestMessageRow(val chatMessage : Message) : Item<GroupieViewHolder>() {
 
         var chatPartnerUser: profile? = null
 
         override fun bind(viewHolder: GroupieViewHolder, position: Int) {
 
-            viewHolder.itemView.username_textview_latest_message.text = chatMessage.text
+            val latestMessage = viewHolder.itemView.findViewById<TextView>(R.id.latest_message_textview)
+            val latestUsername = viewHolder.itemView.findViewById<TextView>(R.id.username_textview_latest_message)
+
+            latestMessage.text = chatMessage.text.toString()
 
             val chatPartnerId: String
             if (chatMessage.fromId == FirebaseAuth.getInstance().uid) {
@@ -99,7 +105,7 @@ class LatestMessageActivity : AppCompatActivity() {
             }
 
             val reference = FirebaseFirestore.getInstance()
-                .collection("users_chat")
+                .collection("users")
                 .document(chatPartnerId)
 
             reference.addSnapshotListener { querySnapshot, e ->
@@ -107,19 +113,17 @@ class LatestMessageActivity : AppCompatActivity() {
                 if (querySnapshot != null) {
 
                     chatPartnerUser = profile(
-                        querySnapshot.data?.getValue("dtaNasc").toString(),
+                        querySnapshot.data?.getValue("uid").toString(),
                         querySnapshot.data?.getValue("nome").toString(),
-                        querySnapshot.data?.getValue("uid").toString()
+                        querySnapshot.data?.getValue("dtaNasc").toString()
                     )
 
-                    viewHolder.itemView.username_textview_latest_message.text =
-                        chatPartnerUser!!.nome
-
+                    latestUsername.text = chatPartnerUser!!.nome.toString()
                 }
             }
         }
         override fun getLayout(): Int {
-            return R.layout.activity_latest_message
+            return R.layout.chat_ultimas_mensagens
         }
     }
     private fun listenForLatestMessages() {
@@ -136,7 +140,7 @@ class LatestMessageActivity : AppCompatActivity() {
 
                 for (d in querySnapshot) {
 
-                    val chatMessage = com.example.ipca.gamecatalog.alldayclinic.Message(
+                    val chatMessage = Message(
                         d.data.getValue("text").toString(),
                         d.data.getValue("fromId").toString(),
                         d.data.getValue("toId").toString(),
