4f9f197ff94593145a49f38ecd00d1ff0c398898
SirNoura <65782557+SirNoura@users.noreply.github.com>
2021-01-15 15:07:47 +0000

    pequena alteraçao no design

--- app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/ui/chat/LatestMessageActivity.kt
+++ app/src/main/java/com/example/ipca/gamecatalog/alldayclinic/ui/chat/LatestMessageActivity.kt
@@ -38,6 +38,7 @@ class LatestMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_latest_message)
+        supportActionBar?.hide()
 
         var btn_nova = findViewById<Button>(R.id.btnmensagemnova)
         btn_nova.setOnClickListener {
@@ -62,27 +63,6 @@ class LatestMessageActivity : AppCompatActivity() {
         fetchCurrentUser()
     }
 
-    //Instruções após clicar em algum dos botões
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.new_message_button -> {
-                val intent = Intent(this, NewMessageActivity::class.java)
-                startActivity(intent)
-            }
-            R.id.signout_button -> {
-                FirebaseAuth.getInstance().signOut()
-                val intent = Intent(this, MainActivity::class.java)
-                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
-            }
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    //criar o menu na parte de cima da app
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.nav_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
 
     class LatestMessageRow(val chatMessage : Message) : Item<GroupieViewHolder>() {
 
