1374ca806ca70b8d111a5cc194f26d03b79342a4
rowst07 <51093341+rowst07@users.noreply.github.com>
2021-01-19 00:20:50 +0000

    Registo + Firestore feito

--- website/public/scripts/auth.js
+++ website/public/scripts/auth.js
@@ -1,18 +1,3 @@
-firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-      var uid = user.uid;
-      //É direcionado para a Página Inicial
-      document.location.replace("../inicial.html")
-      // ...
-    } else {
-      // User is signed out
-      // ...
-      document.location.replace("../login.html")
-
-    }
-  });
 
 
 
@@ -51,36 +36,35 @@ function registo(){
     var dataNasc = document.getElementById("meterAno").value;
 
     var db = firebase.firestore()
+    var userid = firebase.auth().currentUser.uid;
 
     firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((user) => {
       // Signed in
-      var userid = user.uid;
-      
-      db.collection("users").doc(userid).set({
-        dtaNasc: dataNasc,
-        nome: nomeUser,
-        uid: userid
+
+        db.collection("users").doc(firebase.auth().currentUser.uid).set({
+            dtaNasc: dataNasc,
+            nome: nomeUser,
+            uid: firebase.auth().currentUser.uid
         })
         .then(function() {
-            window.alert("Utilizador criado com sucesso");
-
-            // Abre a Página Login
-            document.location='login.html'
+            document.location.replace("login.html")
         })
         .catch(function(error) {
-            window.alert("Dados não introduzidos no Firestore");
-        });
+            console.error("Erro a registar no Firestore: ", error);
+    });
+      
     })
     .catch((error) => {
       var errorCode = error.code;
       var errorMessage = error.message;
       // Aparece no ecrã que ocorreu um erro ao criar a conta
 
-      window.alert("Ocorreu um erro ao criar a conta. Verifique se as informações estão corretas.");
+      console.log("Não foi possível criar a conta.");
       console.log(error);
-    });
+    }); 
 
+      
 }
 
 function logout(){
@@ -92,4 +76,23 @@ function logout(){
         // An error happened.
         window.alert("Ocorreu um erro a terminar a sessão.");
       });
+}
+
+function lerDados(){
+    var db = firebase.firestore()
+    var userid = firebase.auth().currentUser.uid;
+
+    var docRef = db.collection("users").doc(userid);
+
+    docRef.get().then(function(doc) {
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+});
+
 }
